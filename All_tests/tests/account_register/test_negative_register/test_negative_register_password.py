495053c79ce37be6e40c03930e9fc40105faa0d3
Fordel <hostink325@gmail.com>
2026-05-05 18:43:51 +0300

    Update

---
+++ All_tests/tests/account_register/test_negative_register/test_negative_register_password.py
@@ -0,0 +1,45 @@
+import allure
+
+from playwright.sync_api import expect
+
+
+@allure.title("Регистрация нового пользователя")
+def tests_registration(page):
+    account_username = "fordelZ"
+    account_password = ""
+    account_email = "fordel1@mail.ru"
+
+
+    with allure.step('1. Переход на страницу сайта регистрации'):
+        page.goto("https://pizzeria.skillbox.cc/register/")
+
+    with allure.step("2. Вводим email и пароль верно, поле имя оставляем пустым"):
+        username_input = page.locator('#reg_username')
+        username_input.fill(account_username)
+
+        email_input = page.locator('#reg_email')
+        email_input.fill(account_email)
+
+        password_input = page.locator('#reg_password')
+        password_input.fill(account_password)
+
+        apply_register_button = page.locator('button[name="register"]')
+        apply_register_button.click()
+
+    with allure.step("3. Нажимаем кнопку подтвердить регистрацию"):
+        apply_register_button = page.locator('button[name="register"]')
+        apply_register_button.click()
+
+    with allure.step("Проверяем что выдало ошибку в поле Пароль"):
+        error_message = page.get_by_role('alert')
+        expect(error_message).to_be_visible(timeout=10000)
+        print("Ошибка ввода имени обнаружена")
+
+        error_text = error_message.text_content()
+        error_message_text = error_text.strip()
+
+        allure.attach(
+            f"Ошибка ввода пароля обнаружена\n"
+            f"Необходимо заполнить поле - Пароль "
+        )
+        print(error_message_text)
---
+++ All_tests/tests/bonus_programm/test_bonus_program_negative_name.py
@@ -0,0 +1,44 @@
+import allure
+from playwright.sync_api import expect
+
+
+@allure.title("Флоу на бонусную программу: ")
+def test_bonus_actions_negative(page):
+    test_name = ""
+    test_phone = "89008007060"
+
+
+    with allure.step("1. Переходим на страницу сайта "):
+        page.goto("https://pizzeria.skillbox.cc/", wait_until="load")
+        bonus = page.locator('#menu-item-363 a')
+        expect(bonus).to_be_visible(timeout=10000)
+        bonus.click()
+
+    with allure.step("2. Заполняем данные"):
+        name_field = page.locator('#bonus_username')
+        expect(name_field).to_be_visible(timeout=10000)
+        name_field.fill(test_name)
+
+        phone_field = page.locator('#bonus_phone')
+        expect(phone_field).to_be_visible(timeout=10000)
+        phone_field.fill(test_phone)
+
+    with allure.step("3. Нажимаем на кнопку оформить карту"):
+        accept_bonus = page.get_by_text('Оформить карту')
+        expect(accept_bonus).to_be_visible(timeout=10000)
+        accept_bonus.click()
+        page.wait_for_timeout(2000)
+
+    with allure.step("4. Проверяем что выдало ошибку в поле Имя"):
+        error_message = page.locator('#bonus_content')
+        expect(error_message).to_be_visible(timeout=10000)
+        print("Ошибка ввода имени обнаружена")
+
+        error_text = error_message.text_content()
+        error_message_text = error_text.strip()
+
+        allure.attach(
+            f"Ошибка ввода Имя обнаружена\n"
+            f"Необходимо заполнить поле - Имя "
+        )
+        print(error_message_text)
